ded2d52cf7f671643799253dc2c83a722b047da1
Bernard0101 <bernardogeocometto@gmail.com>
2024-12-19 18:44:26 -0300

    primo commit proggeto algoritmi matrice aggiunto con editore per immagini

---
+++ Algoritmi_Matrice.h
@@ -0,0 +1,30 @@
+#ifndef ALGORITMI_MATRICE_H
+#define ALGORITMI_MATRICE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//la struttura principale della matrice
+typedef struct matrice{
+    float *mat;
+    int lin;
+    int col;
+}matrice;
+
+// Creazione e deallocazione
+void costruire_matrice(matrice *matPtr, int n, int m);
+void costruire_matrice_random(matrice *matPtr, int n, int m);
+void dealocare_matrice(matrice *matPtr);
+
+// Stampa
+void stampare_matrice(matrice *matPtr);
+
+// Operazioni sulle matrici
+void somare_matrice(matrice *matPtr, matrice *matSum, float num);
+void sotrarre_matrice(matrice *matPtr, matrice *matSub, float num);
+matrice *moltiplicare_matrice(matrice *matPtr, matrice *matMul, float num);
+
+//operazioni deverse
+matrice *applicare_filtro(matrice *matPtr, matrice *kernel);
+
+#endif //ALGORITMI_MATRICE_H
